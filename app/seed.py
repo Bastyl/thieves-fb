e19e229eefa8ee6c5efe151ddf7fc22a4481089b
Bastyl <bastian.lopez@mail.udp.cl>
2018-06-14 23:33:30 -0400

    Update seed.py

--- app/seed.py
+++ app/seed.py
@@ -3,30 +3,35 @@ import psycopg2
 conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
 cur = conn.cursor()
 
-sql ="""
-insert into categorias (nombre) values ('Tecnologia '),('Video Juegos '),('Geek'),
-('Cine'),('Mundo Marvel');
+sql = """
+insert into DELNCUENTES (rut,nombre,apellido_paterno,apellido_materno,edad) values
+ ('190323323','bastian,'lopez','orellana','23')
+returning id;
 """
 cur.execute(sql)
-sql ="""
-insert into posts (titulo,resumen,texto,creado) values ('Iron Man 4 ','La nueva pelicula de iron saldra el proximo 2018',
-'Esta pelicula bla bla bla y ser la mejor por que si ',now()) returning id;
+
+sql = """
+insert into CARACTERISTICAS (usuario_id,sexo,estatura,contextura_fisica,
+           tez_piel,nacionalidad,tono_voz,color_pelo,color_ojos,tatuajes) values
+           ('1','masculino','183','atletica','morena','chilena','grave','negro','negro','no')
+returning id;
 """
-cur.execute(sql)
-conn.commit()
-post_id = cur.fetchone()[0]
-
-print post_id
-
-
-sql ="""insert INTO categorias_posts (categoria_id,post_id)
-(SELECT id,%i  FROM categorias where nombre = 'Cine' or 
- nombre = 'Geek' or 
-  nombre = 'Mundo Marvel'
-);"""%(post_id)
 
 cur.execute(sql)
 
+sql = """
+insert into HISTORIAL (usuario_id, sector, relato, fecha, hora) values
+ ('1','vitacura','MME ROBARRRON EN LA CALLEEE','03022018','1650')
+ returning id;
+ """
+
+sql = """
+insert into HISTORIAL (usuario_id, sector, relato, fecha, hora) values
+ ('1','quilicura','MME ROBARRRON EN LA CALLEEE denuvo','04022018','150')
+ returning id;
+ """
+
+cur.execute(sql)
 
 conn.commit()
 cur.close()

43cd6772e5f6242741a1aff53ab8378aa80a61f3
Bastyl <bastian.lopez@mail.udp.cl>
2018-06-14 23:02:38 -0400

    Update setup.py

--- app/setup.py
+++ app/setup.py
@@ -1,52 +1,33 @@
 from configuraciones import *
 import psycopg2
 conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
-
-
 cur = conn.cursor()
-sql ="""DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;"""
 
-cur.execute(sql)
-
-sql ="""
-CREATE TABLE posts 
-           (id serial PRIMARY KEY, titulo varchar(40), resumen varchar, texto text, creado timestamp);
-"""
-
-cur.execute(sql)
+sql ="""DROP TABLE DELINCUENTES;
+        DROP TABLE CARACTERISTICAS;
+        DROP TABLE HISTORIAL;
+        DROP TABLE SECTOR;
 
 
-sql ="""
-CREATE TABLE categorias 
-           (id serial PRIMARY KEY, nombre varchar(40), creado timestamp);
 """
 
 cur.execute(sql)
 
 sql ="""
-CREATE TABLE categorias_posts 
-           (categoria_id integer, post_id integer);
+CREATE TABLE DELINCUENTES 
+           (id serial PRIMARY KEY, rut integer, nombre varchar, apellido_paterno varchar, apellido_materno varchar, edad integer);
+CREATE TABLE CARACTERISTICAS 
+           (id serial PRIMARY KEY, usuario_id integer, sexo varchar, estatura_min integer, estatura_max integer, contextura_fisica varchar,
+           tez_piel varchar, nacionalidad varchar, tono_voz varchar,color_pelo varchar,color_ojos varchar,tatuajes varchar);
+CREATE TABLE HISTORIAL 
+           (id serial PRIMARY KEY, usuario_id integer, sector_id integer, relato var_char, fecha integer, hora integer);
+CREATE TABLE SECTOR 
+           (id serial PRIMARY KEY, zona varchar, lugar_fisico varchar);
 """
 
-cur.execute(sql)
 
-sql ="""
-CREATE TABLE  usuarios
-           (id serial PRIMARY KEY,rol integer, nombre varchar(40),apellido varchar(40),
-           email varchar(100),passwd varchar(255), creado timestamp);
-"""
 
 cur.execute(sql)
-
-sql ="""
-CREATE TABLE comentarios
-           (id serial PRIMARY KEY, comentario varchar(140), post_id integer, usuario_id integer, creado timestamp);
-"""
-
-cur.execute(sql)
-
-
 conn.commit()
 cur.close()
 conn.close()

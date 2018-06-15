4caa0a1a3d18f2c6aebefa0fe1c772a032702b8d
Bastyl <bastian.lopez@mail.udp.cl>
2018-06-14 23:33:44 -0400

    Update setup.py

--- app/setup.py
+++ app/setup.py
@@ -17,12 +17,10 @@ sql ="""
 CREATE TABLE DELINCUENTES 
            (id serial PRIMARY KEY, rut integer, nombre varchar, apellido_paterno varchar, apellido_materno varchar, edad integer);
 CREATE TABLE CARACTERISTICAS 
-           (id serial PRIMARY KEY, usuario_id integer, sexo varchar, estatura_min integer, estatura_max integer, contextura_fisica varchar,
+           (id serial PRIMARY KEY, usuario_id integer, sexo varchar, estatura integer, contextura_fisica varchar,
            tez_piel varchar, nacionalidad varchar, tono_voz varchar,color_pelo varchar,color_ojos varchar,tatuajes varchar);
 CREATE TABLE HISTORIAL 
-           (id serial PRIMARY KEY, usuario_id integer, sector_id integer, relato var_char, fecha integer, hora integer);
-CREATE TABLE SECTOR 
-           (id serial PRIMARY KEY, zona varchar, lugar_fisico varchar);
+           (id serial PRIMARY KEY, usuario_id integer, sector varchar, relato text, fecha integer, hora integer);
 """
 
 

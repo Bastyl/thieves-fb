f91f3ae98f109454ab35c4b7702f1def0105c488
Bastyl <bastian.lopez@mail.udp.cl>
2018-06-18 01:19:45 -0400

    Update setup.py

--- app/setup.py
+++ app/setup.py
@@ -6,21 +6,14 @@ cur = conn.cursor()
 sql ="""DROP TABLE DELINCUENTES;
         DROP TABLE CARACTERISTICAS;
         DROP TABLE HISTORIAL;
-        DROP TABLE SECTOR;
-
-
 """
 
 cur.execute(sql)
 
 sql ="""
-CREATE TABLE DELINCUENTES 
-           (id serial PRIMARY KEY, rut integer, nombre varchar, apellido_paterno varchar, apellido_materno varchar, edad integer);
-CREATE TABLE CARACTERISTICAS 
-           (id serial PRIMARY KEY, usuario_id integer, sexo varchar, estatura integer, contextura_fisica varchar,
-           tez_piel varchar, nacionalidad varchar, tono_voz varchar,color_pelo varchar,color_ojos varchar,tatuajes varchar);
-CREATE TABLE HISTORIAL 
-           (id serial PRIMARY KEY, usuario_id integer, sector varchar, relato text, fecha integer, hora integer);
+CREATE TABLE delincuentes (id serial PRIMARY KEY, rut integer, nombre varchar, apellido_paterno varchar, apellido_materno varchar, edad integer);
+CREATE TABLE caracteristicas (id serial PRIMARY KEY, usuario_id integer, sexo varchar, estatura integer, contextura_fisica varchar,tez_piel varchar, nacionalidad varchar, tono_voz varchar,color_pelo varchar,color_ojos varchar,tatuajes varchar);
+CREATE TABLE historial(id serial PRIMARY KEY, usuario_id integer, sector varchar, relato text, fecha integer, hora integer);
 """
 
 
